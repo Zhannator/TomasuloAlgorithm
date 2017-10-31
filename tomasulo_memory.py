e19da963ee7023ec0af971c3deec403af793d506
Zhannator <zhp3@pitt.edu>
2017-10-30 22:51:30 -0400

    Added encoding to compile with Python2.7

---
+++ tomasulo_memory.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+# Global Variables
+memory = [] # needs to be initialized, 256B(64W), needs its own function on how to reference locations in mem by word/byte
+
+############################################################################################################
+# MEMORY MANAGEMENT
+############################################################################################################		
+def memory_initialize():
+    # code to initialize memory
+    print ("test")
+
+def memory_store_at_addr(addr, value):
+	# code to store value at addr
+    print ("test")
+    
+def memory_load_from_addr(addr):
+	# code to load from addr
+    print ("test")
+############################################################################################################
+
+
+if __name__ == "__main__":
+    #main(sys.argv[1:])
+    main()
