f72b0c3bd805a2f4f410baea3ab5e930ef2f2a13
bfshea <bradshea28@gmail.com>
2017-11-06 20:51:53 -0500

    added pop for load-store

--- tomasulo_load_store_queue.py
+++ tomasulo_load_store_queue.py
@@ -6,7 +6,8 @@
 
 load_store_queue = []
 
-def load_store_queue_add(.....):
+def load_store_queue_add(load_store, load_store_pc, load_store_seq, load_store_addr, load_store_value):
+    #add an entry in the queue if its not full
 
     if len(load_store_queue) < num_load_store_rs:
         load_store_queue_entry = { 
@@ -16,19 +17,31 @@ def load_store_queue_add(.....):
             "addr" : 0,
             "value" : 0 }
         load_store_queue.append(load_store_queue_entry.copy())
+        
+        #update the fields
+        load_store_queue[0]["l/s"] = load_store
+        load_store_queue[0]["pc"] = load_store_pc
+        load_store_queue[0]["seq"] = load_store_seq
+        load_store_queue[0]["addr"] = load_store_addr
+        load_store_queue[0]["value"] = load_store_value
+    
     else:
         print "load-store queue full!"
         return -1
+ 
+ 
+load_store_queue_pop():
+    #pop the oldest instruction from the queue once it completes
     
+    load_store_queue.pop()
 
-def load_store_queue_update(load_store_instr, num_load_store_rs):
-    #Must be performed in order
-    #load_store_instr  -> string for the instruction that was fetched
-    #num_load_store_rs -> number of reservation stations for load-store queue
+def load_store_queue_update(load_store_value, load_store_index):
+    #load_store_value  -> update value or address if it changes
+    #load_store_index  -> index for which row to update in the queue
     
-    load_store_queue[load_store_index]["l/s"] = load_store_instr
-    load_store_index = load_store_index + 1
+    load_store_queue[load_store_index]["value"] = load_store_value
     
     
-        
+if __name__ == "__main__":
+    main()        
 
